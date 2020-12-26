16a2e0942165737a848c349e453cb3d81e9784d4
bj <bj.qq.com>
2020-12-26 14:11:31 +0800

    提交的第一个git管理的maven工程

---
+++ src/main/java/com/bjpower/git/Demo.java
@@ -0,0 +1,11 @@
+package com.bjpower.git;
+
+/**
+ * Qisong
+ * 2020/12/26
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Git demo !");
+    }
+}
